db55c97b4fc49c95ce7145e526bdc215f3363538
aoaow <76165158+aoaow@users.noreply.github.com>
2024-09-11 23:16:48 -0400

    1

--- pandas_test.py
+++ pandas_test.py
@@ -3,5 +3,16 @@ import pandas as pd
 df = pd.read_csv("data/egrid2016.csv")
 print(df.describe())
 
-from matplotlib.pyplot import plt
+import matplotlib.pyplot as plt
+
+plt.plot(df["PLCO2EQA"], df["PLPRMFL"])
+plt.xlabel('PLCO2EQA')
+plt.ylabel('PLPRMFL')
+plt.title('example plot')
+plt.grid(True)
+plt.show()
+
+from ydata_profiling import ProfileReport
+profile = ProfileReport(df, title="profile report")
+profile.to_notebook_ifame()
 
