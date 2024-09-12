504f4cb686637ca390fd7c50c57849c85be4db12
aoaow <76165158+aoaow@users.noreply.github.com>
2024-09-11 23:37:08 -0400

    Update pandas_test.py

--- pandas_test.py
+++ pandas_test.py
@@ -1,18 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv("data/egrid2016.csv")
+df = pd.read_csv("egrid2016.csv")
 print(df.describe())
 
 import matplotlib.pyplot as plt
 
-plt.plot(df["PLCO2EQA"], df["PLPRMFL"])
-plt.xlabel('PLCO2EQA')
-plt.ylabel('PLPRMFL')
-plt.title('example plot')
-plt.grid(True)
+plt.scatter(df["CAPFAC"], df["PLNGENAN"])
 plt.show()
 
 from ydata_profiling import ProfileReport
 profile = ProfileReport(df, title="profile report")
-profile.to_notebook_ifame()
-profile.to_file("egrid_data.pdf")
+profile.to_notebook_iframe()
+profile.to_file("egrid_data.html")
